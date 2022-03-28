f5ffe59baa617e46feaf32c6ddcd79132b884d62
Arad Bin <aradbin@gmail.com>
2022-03-28 22:41:02 +0600

    Layout

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -4,11 +4,11 @@
     <div class="md:flex md:flex-col">
       <div class="md:flex md:flex-col md:h-screen">
         <div class="md:flex md:flex-shrink-0">
-          <div class="flex items-center justify-between px-6 py-4 bg-indigo-900 md:flex-shrink-0 md:justify-center md:w-56">
-            <Link class="mt-1" href="/">
+          <div class="flex items-center justify-between px-6 py-3 bg-indigo-900 md:flex-shrink-0 md:justify-center">
+            <Link class="mt-0" href="/">
               <logo class="fill-white" width="120" height="28" />
             </Link>
-            <dropdown class="md:hidden" placement="bottom-end">
+            <!-- <dropdown class="md:hidden" placement="bottom-end">
               <template #default>
                 <svg class="w-6 h-6 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
               </template>
@@ -17,10 +17,11 @@
                   <main-menu />
                 </div>
               </template>
-            </dropdown>
+            </dropdown> -->
           </div>
-          <div class="md:text-md flex items-center justify-between p-4 w-full text-sm bg-white border-b md:px-12 md:py-0">
-            <div class="mr-4 mt-1">Value Investment</div>
+          <div class="md:text-md flex items-center justify-between px-6 py-4 w-full text-sm bg-white border-b md:px-12 md:py-0">
+            <!-- <div class="mr-4 mt-1">Value Investor BD</div> -->
+            <main-menu class="flex p-0 bg-white overflow-y-auto" />
             <dropdown class="mt-1" placement="bottom-end" v-if="auth.user">
               <template #default>
                 <div class="group flex items-center cursor-pointer select-none">
@@ -58,7 +59,6 @@
           </div>
         </div>
         <div class="md:flex md:flex-grow md:overflow-hidden">
-          <main-menu class="hidden flex-shrink-0 p-6 w-56 bg-indigo-800 overflow-y-auto md:block" />
           <div class="px-4 py-8 md:flex-1 md:p-12 md:overflow-y-auto" scroll-region>
             <flash-messages />
             <slot />
--- resources/js/Shared/MainMenu.vue
+++ resources/js/Shared/MainMenu.vue
@@ -1,40 +1,11 @@
 <template>
   <div>
-    <div class="mb-4">
-      <Link class="group flex items-center py-3" href="/">
-        <icon name="dashboard" class="mr-2 w-4 h-4" :class="isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Dashboard</div>
+    <div class="mb-0">
+      <Link class="group flex items-center" href="/organizations">
+        <icon name="office" class="mr-2 w-4 h-4" :class="isUrl('organizations') ? 'fill-indigo-600' : 'fill-gray-700 group-hover:fill-indigo-600'" />
+        <div class="font-semibold" :class="isUrl('organizations') ? 'text-indigo-600' : 'text-gray-700 group-hover:text-indigo-600 focus:text-indigo-600'">Organizations</div>
       </Link>
     </div>
-    <!-- <div class="mb-4">
-      <Link class="group flex items-center py-3" href="/organizations?account=DSE">
-        <icon name="office" class="mr-2 w-4 h-4" :class="isUrl('organizations?account=DSE') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('organizations?account=DSE') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">DSE</div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="w-5 h-5 fill-white ml-2 mt-1"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path></svg>
-      </Link>
-      <div class="transition ease-in duration-700 p-5 mt-3 bg-indigo-900 flex flex-col">
-        <Link class="items-center mb-5" :class="isUrl('organizations?account=DSE') ? 'text-white' : 'text-indigo-300 group-hover:text-white'" href="/organizations?account=DSE">DSEX</Link>
-        <Link class="items-center" :class="isUrl('organizations?account=DSE&index=DS30') ? 'text-white' : 'text-indigo-300 group-hover:text-white'" href="/organizations?account=DSE&index=DS30">DS30</Link>
-      </div>
-    </div> -->
-    <div class="mb-4">
-      <Link class="group flex items-center py-3" href="/organizations">
-        <icon name="office" class="mr-2 w-4 h-4" :class="isUrl('organizations') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('organizations') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Organizations</div>
-      </Link>
-    </div>
-    <!-- <div class="mb-4">
-      <Link class="group flex items-center py-3" href="/contacts">
-        <icon name="users" class="mr-2 w-4 h-4" :class="isUrl('contacts') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Contacts</div>
-      </Link>
-    </div>
-    <div class="mb-4">
-      <Link class="group flex items-center py-3" href="/reports">
-        <icon name="printer" class="mr-2 w-4 h-4" :class="isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'" />
-        <div :class="isUrl('reports') ? 'text-white' : 'text-indigo-300 group-hover:text-white'">Reports</div>
-      </Link>
-    </div> -->
   </div>
 </template>
 
