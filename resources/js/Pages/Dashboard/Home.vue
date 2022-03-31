9c0485968bdafdec069fd744cc76e8fd73f18103
Arad Bin <aradbin@gmail.com>
2022-03-31 15:28:34 +0600

    Homepage

---
+++ resources/js/Pages/Dashboard/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Head title="Home" />
+    <img class="w-full" src="/images/quote-1.jpg" />
+  </div>
+</template>
+
+<script>
+import { Head } from '@inertiajs/inertia-vue3'
+import Layout from '@/Shared/Layout'
+export default {
+  components: {
+    Head,
+  },
+  layout: Layout,
+}
+</script>
