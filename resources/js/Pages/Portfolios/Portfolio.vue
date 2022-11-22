4ef4b52116a2bec5059e68dcd57b41e0b888a329
Arad bin <arad.siddiqui@upaybd.com>
2022-11-22 17:13:44 +0600

    toFixed issue fixed

--- resources/js/Pages/Portfolios/Portfolio.vue
+++ resources/js/Pages/Portfolios/Portfolio.vue
@@ -8,7 +8,7 @@
     <div class="grid grid-cols-4 sm:grid-cols-2 shadow">
       <div class="text-center px-3 py-2 font-bold text-white border border-solid" :class="store.portfolio?.value < 0 ? 'bg-red-600' : 'bg-green-600'">
         <span class="block text-center text-xs">Portfolio Value</span>
-        <h6 class="text-center text-2xl">{{ store.portfolio?.value ? store.portfolio?.value.toFixed(2) : 0 }}</h6>
+        <h6 class="text-center text-2xl">{{ store.portfolio?.value ? store.portfolio?.value : 0 }}</h6>
       </div>
       <div class="text-center px-3 py-2 font-bold text-white border border-solid" :class="(!store.portfolio?.gain || store.portfolio?.gain < 0) ? 'bg-red-600' : 'bg-green-600'">
         <span class="block text-center text-xs">Gain (%)</span>
@@ -20,7 +20,7 @@
       </div>
       <div class="text-center px-3 py-2 font-bold text-white border border-solid" :class="store.portfolio?.value + store.portfolio?.balance < 0 ? 'bg-red-600' : 'bg-green-600'">
         <span class="block text-center text-xs">Total Assets</span>
-        <h6 class="text-center text-2xl">{{ store.portfolio?.value ? (store.portfolio?.value + store.portfolio?.balance).toFixed(2) : store.portfolio?.balance }}</h6>
+        <h6 class="text-center text-2xl">{{ store.portfolio?.value ? (store.portfolio?.value + store.portfolio?.balance) : store.portfolio?.balance }}</h6>
       </div>
     </div>
     <div class="mt-4 mb-4">
