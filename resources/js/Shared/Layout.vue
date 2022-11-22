29abd824cf40b19ad1422e5755175ef7d8e0222d
Arad bin <arad.siddiqui@upaybd.com>
2022-11-22 18:26:42 +0600

    Continous update on market hour

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -119,12 +119,12 @@ export default {
   },
   mounted(){
     this.getOrganizations();
-    // window.setInterval(() => {
-    //   let d = new Date();
-    //   if(d.getDay()<5 && d.getHours()>10 && d.getHours()<15){
-    //     this.getOrganizations(true);
-    //   }
-    // }, 60000);
+    window.setInterval(() => {
+      let d = new Date();
+      if(d.getDay()<5 && d.getHours()>10 && d.getHours()<15){
+        this.getAllOrganizations();
+      }
+    }, 60000);
   }
 }
 </script>
